4dd35e730dd12dde90b16672600395d5387d6e42
MusayevDoniyor <doniyormusayev2009@gmail.com>
2025-02-03 19:48:18 +0500

    fix error

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
-export default function generateUniqueId() {
+function generateUniqueId() {
   return "id-" + Date.now() + "-" + Math.floor(Math.random() * 1000);
 }
+
+module.exports = generateUniqueId;
